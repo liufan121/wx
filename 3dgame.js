501e58241495dc0ca2a15c13435aac12209c0046
liufan <842796155@qq.com>
2018-05-23 17:48:17 +0800

    11
    11

--- 3dgame.js
+++ 3dgame.js
@@ -1,76 +1,68 @@
 import * as THREE from './three/three.js';
-import  './three/OrbitControls.js';
 import './three/STLLoader.js';
 export default class game3d {
     constructor() {
       //场景
       this.scene=new THREE.Scene();
       //相机
-      this.camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000);
-      this.renderer=new THREE.WebGLRenderer({
-        canvas:canvas
-      });
-      this.renderer.setSize(window.innerWidth,window.innerHeight);
+      this.camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,1,1000);
+      this.camera.position.x=10;
+      this.camera.position.y = 10;
+      this.camera.position.z=500; 
+      //渲染
+      this.renderer = new THREE.WebGLRenderer({ canvas:canvas});
+      this.renderer.setClearColor(0xffffff, 1);
       this.start();
-      this.renderer.setClearColor(0xAAFFCC);
     }
-    start() {
-      //生成一个坐标轴，辅助线
-      var axes = new THREE.AxisHelper(20);
-      this.scene.add(axes);
-      //球体
-      var sphereGeometry = new THREE.SphereGeometry(4, 30, 30);
-      var texture = new THREE.TextureLoader().load("images/metal.jpg");  
-      var sphereMaterial = new THREE.MeshBasicMaterial({ map: texture });
-      this.sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-      this.sphere.position.x =0;
-      this.sphere.position.y =0;
-      this.sphere.position.z = 0;
-      this.scene.add(this.sphere);
-
-      //圆台
-      //this.wen();
-      //点光
-      // var spotLight = new THREE.PointLight('#0e71a4');
-      //   spotLight.position.set(-10, 90, 35);
-      //   spotLight.castShadow = true;
-      //this.scene.add(spotLight);
-      this.camera.position.x = 0;
-      this.camera.position.y = 30;
-      this.camera.position.z = 100;
-      this.camera.lookAt(this.scene.position);
-      this.animate();
+    start(){
+      var axisHelper = new THREE.AxisHelper(6);
+      this.scene.add(axisHelper);
+      //创建模型
       var loader = new THREE.JSONLoader();
-      loader.load("images/lr.json", (geometry)=>{
-        var material = new THREE.MeshBasicMaterial({
-          wireframe: true,
-          color: 'red'
-        });
-        var torus = new THREE.Mesh(geometry, material);
-        this.scene.add(torus);
+      var url ='http://www.yanglaohky.cn/images/sekuai.js';
+      loader.load(url, (geometry)=>{
+        var matArray = [];
+        matArray.push(new THREE.MeshBasicMaterial({ color: 0x009e60 }));
+        matArray.push(new THREE.MeshBasicMaterial({ color: 0x0051ba }));
+        matArray.push(new THREE.MeshBasicMaterial({ color: 0xffd500 }));
+        matArray.push(new THREE.MeshBasicMaterial({ color: 0xff5800 }));
+        matArray.push(new THREE.MeshBasicMaterial({ color: 0xc41e3a }));
+        matArray.push(new THREE.MeshBasicMaterial({ color: 0xffffff })); 
+        var material = new THREE.MeshFaceMaterial();
+        var mesh = new THREE.Mesh(geometry, matArray);
+        mesh.position.set(0,0,0);
+        this.scene.add(mesh);
+        this.animate();
       })
-    }
-    wen(){
-      var axis = new THREE.Vector3(0, 0, 1);//向量axis
-      for(var i=0;i<8;i++){
-        var geometry = new THREE.TorusGeometry(20,3,5,10,Math.PI*1/4.5);
-        var material = new THREE.MeshBasicMaterial({ 
-          wireframe:true,
-          color:'red'
-        });
-        var torus = new THREE.Mesh(geometry, material);
-        torus.rotateOnAxis(axis, Math.PI*i/ 4);//绕axis轴旋转π/8
-        //点光
-        this.scene.add(torus);
-      }
-    }
-    animate(){
-      this.sphere.rotation.x -= 0.1;
-      this.renderer.clear();  
-      this.renderer.render(this.scene, this.camera);
-      requestAnimationFrame(this.animate.bind(this));
+      var group = new THREE.Group();
       
     }
+    makeTextSprite(message, parameters) {
+      //创建canvas字体
+        if (parameters === undefined) parameters = {};
+            var fontface = parameters.hasOwnProperty("fontface") ? parameters["fontface"] : "Aria";
+            var fontsize = parameters.hasOwnProperty("fontsize") ? parameters["fontsize"] : 18;
+            var borderThickness = parameters.hasOwnProperty("borderThickness") ? parameters["borderThickness"] : 4;
+            var textColor = parameters.hasOwnProperty("textColor") ? parameters["textColor"] : { r: 0, g: 0, b: 0, a: 1.0 };
 
+            var canvas = document.createElement('canvas');
+            var context = canvas.getContext('2d');
+            context.font =fontsize + "px " + fontface;
+            var metrics = context.measureText(message);
+            var textWidth = metrics.width;
+            context.lineWidth = borderThickness;
+            context.fillStyle = "rgb(" + textColor.r + ", " + textColor.g + ", " + textColor.b+")";
+            context.fillText(message, borderThickness, fontsize + borderThickness);
+            var texture = new THREE.Texture(canvas)
+            texture.needsUpdate = true;
+
+            var spriteMaterial = new THREE.SpriteMaterial({ map: texture, useScreenCoordinates: false });
+            var sprite = new THREE.Sprite(spriteMaterial);
+            sprite.scale.set(0.25 * fontsize, 0.45 * fontsize, 0.25 * fontsize);
+            return sprite;
+    }
+    animate(){
+      this.renderer.render(this.scene, this.camera);
+    }
 }
 
